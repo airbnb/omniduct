e6f3a5b4008b81250e3b1a527138e6dedfff9047
Matthew Wardrop <matthew.wardrop@airbnb.com>
2017-03-09 20:51:31 -0800

    Add classifiers to setup.py.

--- setup.py
+++ setup.py
@@ -9,17 +9,26 @@ with open('omniduct/_version.py') as version_file:
     exec(version_file.read(), version_info)
 
 setup(
-    # Application name:
+    # Package metadata
     name="omniduct",
-    url="https://github.com/airbnb/omniduct",
-    description="A toolkit providing a uniform interface for connecting to and extracting data from a wide variety of (potentially remote) data stores (including HDFS, Hive, Presto, MySQL, etc).",
-
-    # Version number:
     version=version_info['__version__'],
-
-    # Application author details:
     author=version_info['__author__'],
     author_email=version_info['__author_email__'],
+    url="https://github.com/airbnb/omniduct",
+    description="A toolkit providing a uniform interface for connecting to and extracting data from a wide variety of (potentially remote) data stores (including HDFS, Hive, Presto, MySQL, etc).",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+    ],
 
     # Package details
     packages=find_packages(),
