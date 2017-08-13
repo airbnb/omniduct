dbc303d87fd4c2a2509d4a8cba61b3145a411048
Matthew Wardrop <matthew.wardrop@airbnb.com>
2017-08-13 14:10:56 -0700

    Integrate more tightly with Duct system.

--- omniduct/protocols.py
+++ omniduct/protocols.py
@@ -5,6 +5,6 @@ from .databases.sqlalchemy import SQLAlchemyClient
 from .filesystems.local import LocalFsClient
 from .filesystems.webhdfs import WebHdfsClient
 from .remotes.ssh import SSHClient
-from .restful.rest import BasicRestClient
+from .restful.base import RestClient
 
 # from .remotes.ssh_paramiko import ParamikoSSHClient  # Not yet ready for prime time
--- omniduct/restful/base.py
+++ omniduct/restful/base.py
@@ -1,8 +1,11 @@
+import requests
+from future.moves.urllib.parse import urlparse, urlencode, urljoin
+
 from omniduct.duct import Duct
 from omniduct.utils.magics import MagicsProvider, process_line_arguments
 
 
-class RestClient(Duct):
+class RestClientBase(Duct):
     '''
     This is a simple wrapper around the requests library to simplify the use
     of RESTful clients with omniduct. This allows all the automatic features
@@ -12,25 +15,42 @@ class RestClient(Duct):
     to access various endpoints.
     '''
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, server_protocol='http', assume_json=False, endpoint_prefix='', **kwargs):
         '''
         This is a shim __init__ function that passes all arguments onto
         `self._init`, which is implemented by subclasses. This allows subclasses
         to instantiate themselves with arbitrary parameters.
         '''
-        Duct.__init_with_kwargs__(self, kwargs)
+        Duct.__init_with_kwargs__(self, kwargs, port=80)
+
+        self.server_protocol = server_protocol
+        self.assume_json = assume_json
+        self.endpoint_prefix = endpoint_prefix
+
         self._init(*args, **kwargs)
 
-    def _init(self, base_url):
-        self.base_url = base_url
+    def _init(self):
+        pass
 
-    def request(self, method, endpoint, **kwargs):
+    def __call__(self, endpoint, method='get', **kwargs):
+        if self.assume_json:
+            return self.request_json(endpoint, method=method, **kwargs)
+        return self.request(endpoint, method=method, **kwargs)
+
+    @property
+    def base_url(self):
+        url = urljoin('{}://{}:{}'.format(self.server_protocol, self.host, self.port or 80), self.endpoint_prefix)
+        if not url.endswith('/'):
+            url += '/'
+        return url
+
+    def request(self, endpoint, method='get', **kwargs):
         self.connect()
         url = urljoin(self.base_url, endpoint)
         return requests.request(method, url, **kwargs)
 
-    def request_json(self, method, endpoint, **kwargs):
-        request = self.request(method, endpoint, **kwargs)
+    def request_json(self, endpoint, method='get', **kwargs):
+        request = self.request(endpoint, method=method, **kwargs)
         if not request.status_code == 200:
             raise RuntimeError("Server responded with HTTP response code {}, with content: {}.".format(request.status_code, request.content.decode()))
         return request.json()
@@ -44,6 +64,5 @@ class RestClient(Duct):
     def _disconnect(self):
         pass
 
-
-class BasicRestClient(RestClient):
+class RestClient(RestClientBase):
     PROTOCOLS = ['rest']
