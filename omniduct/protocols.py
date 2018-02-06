3a89008751530bb8b908df1bb255b41a2ebcc4e0
Dan Frank <dan.frank@airbnb.com>
2018-02-05 19:38:40 -0800

    add druid client

--- omniduct/protocols.py
+++ omniduct/protocols.py
@@ -3,6 +3,7 @@ from .databases.hiveserver2 import HiveServer2Client
 from .databases.presto import PrestoClient
 from .databases.sqlalchemy import SQLAlchemyClient
 from .databases.neo4j import Neo4jClient
+from .databases.druid import DruidClient
 from .filesystems.local import LocalFsClient
 from .filesystems.s3 import S3Client
 from .filesystems.webhdfs import WebHdfsClient
