18f9560d6439313487a2f27d696b217d1c367592
Matthew Wardrop <mawardrop@netflix.com>
2018-08-22 10:16:09 -0700

    Add two new serializers: BytesSerializer and PandasSerializer.

--- omniduct/caches/_serializers.py
+++ omniduct/caches/_serializers.py
@@ -1,17 +1,34 @@
 import pickle
+from distutils.version import LooseVersion
+
+import pandas
 
 
 class Serializer(object):
 
     @property
     def file_extension(self):
-        return ".{}".format(self.__class__.__name__.lower())
+        return ""
 
     def serialize(self, obj, fh):
         raise NotImplementedError
 
     def deserialize(self, fh):
         raise NotImplementedError
+
+
+class BytesSerializer(Serializer):
+
+    @property
+    def file_extension(self):
+        return ".bytes"
+
+    def serialize(self, obj, fh):
+        assert isinstance(obj, bytes), "BytesSerializer requires incoming data be already encoded into a bytestring."
+        fh.write(obj)
+
+    def deserialize(self, fh):
+        return fh.read()
 
 
 class PickleSerializer(Serializer):
@@ -25,3 +42,22 @@ class PickleSerializer(Serializer):
 
     def deserialize(self, fh):
         return pickle.load(fh)
+
+
+class PandasSerializer(Serializer):
+
+    @property
+    def file_extension(self):
+        return ".pandas"
+
+    @classmethod
+    def serialize(cls, formatted_data, fh):
+        # compat: if pandas is old, to_pickle does not accept file handles
+        if LooseVersion(pandas.__version__) <= LooseVersion('0.20.3'):
+            fh.close()
+            fh = fh.name
+        return pandas.to_pickle(formatted_data, fh)
+
+    @classmethod
+    def deserialize(cls, fh):
+        return pandas.read_pickle(fh)
