24020c58474c2ad7b90ea7858b56a6b6d653df3b
Gauthier Loutsch <gauthier.loutsch@linkfluence.com>
2020-07-08 14:27:14 +0200

    Fix webhdfs client init when no autoconf

--- omniduct/filesystems/webhdfs.py
+++ omniduct/filesystems/webhdfs.py
@@ -56,6 +56,8 @@ class WebHdfsClient(FileSystemClient):
                 return random.choice(duct.namenodes)
 
             self._host = partial(get_host_and_set_namenodes, cluster=auto_conf_cluster, conf_path=auto_conf_path)
+        else:
+            self._host = self.namenodes
 
         self.__webhdfs = None
         self.__webhdfs_kwargs = kwargs
