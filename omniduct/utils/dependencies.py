92427f6fb13eb7dc72cf6cf70e9494c963693664
Matthew Wardrop <mawardrop@netflix.com>
2023-11-01 09:55:43 -0700

    Fix dependency detection in Python 3.12+

--- omniduct/utils/dependencies.py
+++ omniduct/utils/dependencies.py
@@ -1,11 +1,26 @@
 import importlib
 import re
+from typing import Optional
 
-import pkg_resources
-from pkg_resources import VersionConflict
+import packaging.requirements
 
 from omniduct._version import __optional_dependencies__
 from omniduct.utils.debug import logger
+
+
+def get_package_version(package_name: str) -> Optional[str]:
+    """
+    Return the version of the given package, or None if the package is not
+    installed.
+    """
+    try:  # Python 3.8+
+        import importlib.metadata
+
+        return importlib.metadata.version(package_name)
+    except ImportError:  # Python <3.12
+        import pkg_resources
+
+        return pkg_resources.get_distribution(package_name).version
 
 
 def check_dependencies(protocols, message=None):
@@ -25,26 +40,26 @@ def check_dependencies(protocols, message=None):
         package_name = m.group(0)
         accept_any_version = package_name == dep
 
-        try:
-            pkg_resources.get_distribution(dep)
-        except VersionConflict:
-            warning_deps[dep] = "{}=={}".format(
-                package_name, pkg_resources.get_distribution(m.group(0)).version
-            )
-        except:
+        dep_req = packaging.requirements.Requirement(dep)
+        package_name = dep_req.name
+        package_version = get_package_version(package_name)
+
+        if package_version is None:
             # Some packages may be available, but not installed. If so, we
             # should accept them with warnings (if version specified in dep).
             try:
                 importlib.import_module(package_name)
                 if not accept_any_version:
-                    warning_deps.append("{}==<not installed>".format(package_name))
-            except:  # ImportError in python 2, ModuleNotFoundError in Python 3
+                    warning_deps[dep] = f"{package_name}==<not installed>"
+            except ModuleNotFoundError:
                 missing_deps.append(dep)
+        elif dep_req.specifier and not dep_req.specifier.contains(package_version):
+            warning_deps[dep] = f"{package_name}=={package_version}"
 
     if warning_deps:
         message = "You may have some outdated packages:\n"
         for key in sorted(warning_deps):
-            message += "\t- Want {}, found {}".format(key, warning_deps[key])
+            message += f"\t- Want {key}, found {warning_deps[key]}"
         logger.warning(message)
     if missing_deps:
         message = (
@@ -52,8 +67,8 @@ def check_dependencies(protocols, message=None):
         )
         fix = (
             "You can fix this by running:\n\n"
-            "\t{install_command}\n\n"
+            f"\tpip install --upgrade {' '.join(missing_deps)}\n\n"
             "Note: Depending on your system's installation of Python, you may "
-            "need to use `pip2` or `pip3` instead of `pip`."
-        ).format(install_command="pip install --upgrade " + " ".join(missing_deps))
+            "need to use `pip3` instead of `pip`."
+        )
         raise RuntimeError("\n\n".join([message, fix]))
